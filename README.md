c4f9584a373f70baff4225030d25773d2e74b7f7
George Phillips <georgephillips@Georges-MacBook-Air.local>
2021-05-05 16:47:04 +0100

    packaged as pip package

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ## Azure Tools
 This is a library that contains basic azure utilities and tools used by many of the projects in Idio/Optimizely.
+
+### Deploying
+To build a distrobution run:
+python setup.py sdist
+
+To upload:
+twine upload <url> dist/*
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='azure_tools',
+    version='0.0.1',    
+    description='Python tools for working with azure',
+    url='https://github.com/idio/azure_tools',
+    author="Fandango",
+    author_email="george.phillips@episerver.com",
+    packages=find_packages(where="aztools"),
+    install_requires=[
+        "azure-common==1.1.26",
+        "azure-identity==1.5.0",
+        "azure-kusto-data==1.0.3",
+        "azure-storage-blob==12.6.0",
+        "smart_open==3.0.0",
+        "smart_open[azure]==3.0.0",
+    ],
+
+    classifiers=[      
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires=">=3.8"
+)
