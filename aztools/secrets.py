f75d2d35c971a11be85dd969bc23fb929c150233
Efstathios Charitos <stathischaritos@gmail.com>
2022-01-05 13:29:05 +0000

    Created class to interface the azure key vault

---
+++ aztools/secrets.py
@@ -0,0 +1,21 @@
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
+
+class AzureSecretStore():
+
+    def __init__(self, keyVaultName):
+        KVUri = f"https://{keyVaultName}.vault.azure.net"
+        credential = DefaultAzureCredential()
+        self.client = SecretClient(vault_url=KVUri, credential=credential)
+
+    def set_secret(self, secret_name, secret_value):
+        self.client.set_secret(secret_name, secret_value)
+
+    def get_secret(self, secret_name):
+        return self.client.get_secret(secret_name).value
+
+    def delete_secret(self, secret_name):
+        poller = client.begin_delete_secret(secretName)
+        deleted_secret = poller.result()
+        return deleted_secret
