b6e99ef4383d86722dce9a598758eb49e17cc5c8
Efstathios Charitos <stathischaritos@gmail.com>
2022-01-05 13:33:58 +0000

    added bad test (not mocked) and changed var names

--- aztools/secrets.py
+++ aztools/secrets.py
@@ -4,10 +4,10 @@ from azure.identity import DefaultAzureCredential
 
 class AzureSecretStore():
 
-    def __init__(self, keyVaultName):
-        KVUri = f"https://{keyVaultName}.vault.azure.net"
+    def __init__(self, key_vault):
+        key_vault_uri = f"https://{key_vault}.vault.azure.net"
         credential = DefaultAzureCredential()
-        self.client = SecretClient(vault_url=KVUri, credential=credential)
+        self.client = SecretClient(vault_url=key_vault_uri, credential=credential)
 
     def set_secret(self, secret_name, secret_value):
         self.client.set_secret(secret_name, secret_value)
